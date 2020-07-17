af4c5f2d0ca8128935904d64f2bb091845f10e59
Anefu Favour <favouranefu@gmail.com>
2020-07-17 22:31:23 +0100

    Added rename and delete feature; problems with flow control initiated

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,61 @@
-import { readdir } from 'fs';
-import minimist from 'minimist';
+import { readdir, unlink, rename } from "fs";
+import minimist from "minimist";
+import { resolve } from "path";
 
 async function main() {
-    const args = minimist(process.argv.splice(2))
-    const re = new RegExp(args.filer)
-    const matchedFiles = (await allFiles(args.dir)).filter(filename => re.test(filename))
-    console.log(matchedFiles)
+  const args = minimist(process.argv.splice(2), {
+    string: ["remhd"],
+    boolean: ["del"],
+  });
+
+  const pattern = new RegExp(args._[0]);
+  const matchedFiles = (await allFiles(process.cwd())).filter((filename) =>
+    pattern.test(filename)
+  );
+
+  if (args.del) {
+    deleteFiles(matchedFiles.map((filename) => resolve(filename)));
+    console.log(
+      `${matchedFiles.length} files matching pattern ${args._[0]} deleted.`
+    );
+  } else if (args.remhd) {
+    renameFiles(matchedFiles, args.remhd, pattern);
+    console.log(
+      ` ${args._[0]} in ${matchedFiles.length} files changed to ${args.remhd}.`
+    );
+  } else if (matchedFiles.length > 0) {
+    for (const file of matchedFiles) {
+      console.log(file);
+    }
+  } else {
+    console.log("No filename in directory matching pattern: " + args._[0])
+  }
 }
 
 function allFiles(foldername: string): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        readdir(foldername, (err, files) => {
-            if (err) reject(err)
-            else resolve(files)
-        })
-    })
+  return new Promise((resolve, reject) => {
+    readdir(foldername, (err, files) => {
+      if (err) reject(err);
+      else resolve(files);
+    });
+  });
 }
 
-main()
+function renameFiles(files: string[], replacer: string, pattern: RegExp) {
+  for (const file of files) {
+    const newName = resolve(file.replace(pattern, replacer));
+    rename(resolve(file), newName, (err) => {
+      if (err) console.error(err);
+    });
+  }
+}
+
+function deleteFiles(files: string[]) {
+  for (const file of files) {
+    unlink(file, (err) => {
+      if (err) console.error(err);
+    });
+  }
+}
+
+main();
