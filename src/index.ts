97e8032ca02b82db1b2948b2b211eb8161bb51c7
Anefu Favour <favouranefu@gmail.com>
2020-07-17 17:31:00 +0100

    Wrote allFiles function to return an array of names of all files and subdirs of a folder

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("Hello World")
+import { readdir } from 'fs';
+import minimist from 'minimist';
+
+async function main() {
+    const args = minimist(process.argv.splice(2))
+    const re = new RegExp(args.filer)
+    const matchedFiles = (await allFiles(args.dir)).filter(filename => re.test(filename))
+    console.log(matchedFiles)
+}
+
+function allFiles(foldername: string): Promise<string[]> {
+    return new Promise((resolve, reject) => {
+        readdir(foldername, (err, files) => {
+            if (err) reject(err)
+            else resolve(files)
+        })
+    })
+}
+
+main()
